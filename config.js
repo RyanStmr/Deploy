53f8498804c588de5fde826d4e3859bae1640f31
RyanStmr <ryan.s@hotmail.de>
2020-07-18 19:09:00 +0200

    added helmet

--- config.js
+++ config.js
@@ -6,9 +6,8 @@ const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-  connectionString:
-    "postgres://uxddqfbuijtvjf:05f10e8bdf4dec99b13be1569c928999d9a045a5d8474c9c40a6fd6df30bd692@ec2-54-228-209-117.eu-west-1.compute.amazonaws.com:5432/d583aie9npjfma",
-  ssl: isProduction,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: false,
 });
 
 module.exports = { pool };
